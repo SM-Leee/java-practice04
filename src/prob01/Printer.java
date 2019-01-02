868c3390c75ae0c363000eb350e912b3abc175f9
BIT <BIT@DESKTOP-E23PFRN>
2019-01-02 16:18:37 +0900

    190102 16:18 Prob01 Complete

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -1,4 +1,21 @@
 package prob01;
 
 public class Printer	 {
+		
+	public void println(int num) {
+		System.out.println(num);
+	}
+	
+	public void println(String str) {
+		System.out.println(str);
+	}
+	
+	public void println(double dou) {
+		System.out.println(dou);
+	}
+	
+	public void println(boolean boo) {
+		System.out.println(boo);
+	}
+	
 }
